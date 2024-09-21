fd4d4407685d5b44eac5ccc9a993467cc4a965ae
Pinfinity <125202205+pinfinity1@users.noreply.github.com>
2024-09-21 10:09:04 +0330

    fix/ add server.js for deploy

--- app/(AdminLayout)/layout.js
+++ app/(AdminLayout)/layout.js
@@ -4,7 +4,7 @@ export default function AdminLayout({ children }) {
   return (
     <div className="w-full h-screen max-h-screen flex flex-grow justify-center items-center">
       <div className="w-full h-full md:max-w-[680px] xl:max-w-[800px] md:max-h-[90%] xl:max-h-[80%] md:border-2 md:rounded-2xl md:shadow-xl flex flex-col md:flex-row relative overflow-hidden">
-        <div className="md:flex-1 w-full h-[calc(100vh_-_72px)] md:h-auto overflow-y-auto">
+        <div className="md:flex-1 w-full h-[calc(100svh_-_72px)] md:h-auto overflow-y-auto">
           {children}
         </div>
         <SideBar />
