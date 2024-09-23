11d05d03d482e7dbedf9500813d43e980e5f2263
Pinfinity <125202205+pinfinity1@users.noreply.github.com>
2024-09-23 13:25:27 +0330

    fix / api

--- libs/axios.js
+++ libs/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const client = axios.create({
-  baseURL: "http://greenfastfood.cocoadownload.com/api/v1/",
+  baseURL: "https://greenfastfood.cocoadownload.com/api/v1/",
 });
 
 export default client;
